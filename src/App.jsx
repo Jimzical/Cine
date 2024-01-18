1d24027435ede26ed01ac9db98bc5083539dd5fc
Jimzical <arishkumar2003@gmail.com>
2024-01-18 18:13:14 +0530

    basic page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from "./components/Navbar.jsx";
 
 function App() {
+  const [search, setSearch] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
   return (
-    <div className="container">
-      <span className="fs-1 ">HI</span>
-    </div>
+    <Router>
+      <NavBar />    
+      <div className="container text-center">
+        <br/>
+        <br/>
+        <form onSubmit={handleFormSubmit} className="input-group mb-3">
+          <input type="text" className="form-control" placeholder='Search for Movie Recommendations' aria-label="Search" aria-describedby="button-addon2" value={search} onChange={handleInputChange} />
+          <button className="btn btn-primary" type="submit" id="button-addon2" style={{width: '150px'}}>Search</button>
+        </form>
+        <p className="mt-4 lead fs-2 "><span >Searching for:</span> {search}</p>
+      
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <span className='badge fs-3 bg-secondary ms-2 p-3'>CINE Recommender</span>
+                    <Link className="navbar-brand ms-4" to="/">Home</Link>
+                    <Link className="navbar-brand ms-1" to="/recommend">Recommend</Link>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link ms-1" to="/login">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
