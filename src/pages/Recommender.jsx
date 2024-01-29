e703e1ae2cdbf5f04d68459f2d2f2c08833e60e3
Jimzical <arishkumar2003@gmail.com>
2024-01-29 23:43:55 +0530

    reccomender looks good, got css

--- src/pages/Recommender.jsx
+++ src/pages/Recommender.jsx
@@ -1,32 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import './pages.css';
 function Recommender() {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [paddingTop, setPaddingTop] = useState('10%');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:8000/movies?limit=-1');
-        setMovies(response.data.movies);
-      } catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/movies?limit=-1');
+                setMovies(response.data.movies.map((movie, index) => ({ id: index, name: movie.original_title })));
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleOnSearch = (string, results) => {
+        console.log(string, results);
     };
 
-    fetchData();
-  }, []);
+    const handleOnSelect = (item) => {
+        setSelectedMovie(item.name);
+        setPaddingTop('2%');
+    };
 
-  return (
-    <div className='text-center mb-5 lead display-4'>
-      Recommendations
-    <div>
-        {movies.map((movie, index) => (
-            <p key={index}>{movie.original_title}</p>
-        ))}
-    </div>
-    </div>
-  );
+    const formatResult = (item) => {
+        return (
+            <>
+                <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+            </>
+        );
+    };
+
+    return (
+        <div className='text-center mb-5 pt-2  lead display-4'>
+            Recommendations
+            <div className='d-flex justify-content-center align-items-center' style={{ paddingTop, transition: 'padding-top 0.5s' }}>
+                <div style={{ width: 400 }}>
+                    <ReactSearchAutocomplete
+                        items={movies}
+                        onSearch={handleOnSearch}
+                        onSelect={handleOnSelect}
+                        autoFocus
+                        formatResult={formatResult}
+                    />
+                </div>
+            </div>
+            {selectedMovie && <p className='text-center h4 mt-3' style={{ animation: 'fadeIn 1s' }}>Movie Selected: {selectedMovie}</p>}        </div>
+    );
 }
 
 export default Recommender;
